1311e685edfc103e3a829c49a4b11c3ea281eba4
Salvador Muñoz <sgmunoz@espol.edu.ec>
2026-06-27 19:24:08 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['bopacorp-crm.jointrymyride.com'],
+    allowedHosts: ['bopacorp-web.jointrymyride.com'],
   },
 });
